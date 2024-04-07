fda8f635e3cbb2ae6601823fd3030b83e47be538
amir-kedis <amir.kedis@gmail.com>
2024-04-07 18:41:11 +0200

    style: formating changes

--- code/scheduler.c
+++ code/scheduler.c
@@ -1,12 +1,10 @@
 #include "headers.h"
 
+int main(int argc, char *argv[]) {
+  initClk();
 
-int main(int argc, char * argv[])
-{
-    initClk();
-    
-    //TODO implement the scheduler :)
-    //upon termination release the clock resources.
-    
-    destroyClk(true);
+  // TODO implement the scheduler :)
+  // upon termination release the clock resources.
+
+  destroyClk(true);
 }
