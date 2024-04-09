f646470a0b70a5d569e822f896787d8118204dba
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-04-10 01:34:13 +0200

    feat(DS): Created doubly linked list

---
+++ code/list.c
@@ -0,0 +1,290 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * createList - create a new d_list
+ *
+ * Return: pointer to new list, NULL on failure
+ */
+d_list *createList()
+{
+	d_list *newList;
+
+	newList = malloc(sizeof(*newList));
+	if (!newList)
+		return (NULL);
+
+	newList->size = 0;
+	newList->head = newList->tail = NULL;
+
+	return (newList);
+}
+
+/**
+ * getNode - get the node in a given index
+ * @list: pointer to list
+ * @index: the index of the node
+ *
+ * Return: pointer to node at index
+ */
+d_node *getNode(d_list *list, unsigned int index)
+{
+	d_node *ptr;
+	int dir = FORWARD;
+	unsigned int i = 0;
+
+	if (!list)
+		return (NULL);
+
+	ptr = list->head;
+	if (index > (list->size / 2))
+	{
+		dir = BACKWARD;
+		ptr = list->tail;
+		i = list->size - 1;
+	}
+
+	while (ptr)
+	{
+		if (i == index)
+			return (ptr);
+
+		if (dir == FORWARD)
+		{
+			i++;
+			ptr = ptr->next;
+		}
+		else
+		{
+			i--;
+			ptr = ptr->prev;
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * insertNodeStart - inserts a new node at the beginning of a d_list
+ * @list: pointer to list
+ * @n: value of the new node
+ *
+ * Return: pointer to new node, NULL in failure
+ */
+d_node *insertNodeStart(d_list *list, void *n)
+{
+	d_node *newNode;
+
+	if (!list)
+		return (NULL);
+
+	newNode = malloc(sizeof(*newNode));
+	if (!newNode)
+		return (NULL);
+
+	newNode->data = n, newNode->prev = NULL;
+	newNode->next = list->head;
+
+	if (list->head)
+		list->head->prev = newNode;
+	else
+	  list->tail = newNode;
+	list->head = newNode;
+	list->size++;
+
+	return (newNode);
+}
+
+/**
+ * insertNodeEnd - inserts a new node at the end of a d_list
+ * @list: pointer to list
+ * @n: value of the new node
+ *
+ * Return: pointer to new node, NULL in failure
+ */
+d_node *insertNodeEnd(d_list *list, void *n)
+{
+	d_node *newNode;
+
+	if (!list)
+		return (NULL);
+
+	newNode = malloc(sizeof(*newNode));
+	if (!newNode)
+		return (NULL);
+
+	newNode->data = n, newNode->next = NULL;
+	newNode->prev = list->tail;
+
+	if (list->tail)
+		list->tail->next = newNode;
+	else
+	  list->head = newNode;
+	list->tail = newNode;
+	list->size++;
+
+	return (newNode);
+}
+
+/**
+ * insertNode - inserts a new node at a given position
+ * @list: pointer to list
+ * @index: the index where the new node should be added
+ * @n: value in the new node
+ *
+ * Return: pointet to new node, NULL in failure
+ */
+d_node *insertNode(d_list *list, unsigned int index, void *n)
+{
+	d_node *newNode, *ptr;
+
+	if (!list)
+		return (NULL);
+	if (!index)
+		return (insertNodeStart(list, n));
+	if (index == list->size)
+		return (insertNodeEnd(list, n));
+	if (index > list->size)
+		return (NULL);
+
+	ptr = getNode(list, index);
+	newNode = malloc(sizeof(*newNode));
+	if (!newNode)
+		return (NULL);
+
+	newNode->data = n;
+	newNode->next = ptr;
+	newNode->prev = ptr->prev;
+	newNode->prev->next = newNode;
+	newNode->next->prev = newNode;
+	list->size++;
+
+	return (newNode);
+}
+
+/**
+ * deleteNodeStart - delete the node at the beginning of a d_list
+ * @list: pointer to list
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int deleteNodeStart(d_list *list)
+{
+	d_node *toRemove;
+
+	if (!list)
+		return (0);
+	if (!list->size)
+		return (0);
+
+	toRemove = list->head;
+	list->head = list->head->next;
+	freeNode(toRemove);
+	if (list->head)
+		list->head->prev = NULL;
+	else
+		list->tail = NULL;
+	list->size--;
+
+	return (1);
+}
+
+/**
+ * deleteNodeEnd - delete the node at the end of a d_list
+ * @list: pointer to list
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int deleteNodeEnd(d_list *list)
+{
+	d_node *toRemove;
+
+	if (!list)
+		return (0);
+	if (!list->size)
+		return (0);
+
+	toRemove = list->tail;
+	list->tail = list->tail->prev;
+	freeNode(toRemove);
+	if (list->tail)
+		list->tail->next = NULL;
+	else
+		list->head = NULL;
+	list->size--;
+
+	return (1);
+}
+
+/**
+ * deleteNode - delete the node at given index
+ * @list: pointer to list
+ * @index: the index where the node should be deleted
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int deleteNode(d_list *list, unsigned int index)
+{
+	d_node *toRemove;
+
+	if (!list)
+		return (0);
+	if (index >= list->size)
+		return (0);
+	if (!index)
+		return (deleteNodeStart(list));
+	if (index == list->size - 1)
+		return (deleteNodeEnd(list));
+
+	toRemove = getNode(list, index);
+	toRemove->prev->next = toRemove->next;
+	toRemove->next->prev = toRemove->prev;
+	freeNode(toRemove);
+	list->size--;
+
+	return (-1);
+}
+
+/**
+ * freeNode - free a node
+ * @list: pointer to node
+ */
+void freeNode(d_node *node)
+{
+	if (!node)
+		return;
+
+	node->prev = node->next = NULL;
+	free(node->data);
+	free(node);
+}
+/**
+ * freeList - frees all nodes in a d_list
+ * @list: pointer to list
+ */
+void freeList(d_list *list)
+{
+	d_node *ptr, *prv;
+
+	if (!list)
+		return;
+
+	ptr = prv = list->head;
+	while (ptr)
+	{
+		ptr = ptr->next;
+		freeNode(prv);
+		prv = ptr;
+	}
+	list->head = list->tail = NULL;
+	list->size = 0;
+}
+
+/**
+ * destroyList - destroy a list with all nodes
+ * @list: pointer to list
+ */
+void destroyList(d_list *list)
+{
+	freeList(list);
+	free(list);
+}
---
+++ code/list.h
@@ -0,0 +1,51 @@
+#ifndef LIST
+#define LIST
+
+#include <stdlib.h>
+
+#define FORWARD 1
+#define BACKWARD 2
+
+/**
+ * struct d_node - doubly linked node
+ * @data: integer
+ * @prev: pointer to the previous node
+ * @next: pointer to the next node
+ *
+ * Description: doubly linked list node structure
+ */
+typedef struct d_node
+{
+	void *data;
+	struct d_node *prev;
+	struct d_node *next;
+} d_node;
+
+/**
+ * struct d_list - doubly linked list
+ * @size: size of the linked list
+ * @head: pointer to head node
+ * @tail: pointer to tail node
+ *
+ * Description: doubly linked list structure
+ */
+typedef struct d_list
+{
+	size_t size;
+	d_node *head;
+	d_node *tail;
+} d_list;
+
+d_list *createList();
+d_node *getNode(d_list *list, unsigned int index);
+d_node *insertNodeStart(d_list *list, void *n);
+d_node *insertNodeEnd(d_list *list, void *n);
+d_node *insertNode(d_list *list, unsigned int index, void *n);
+int deleteNodeStart(d_list *list);
+int deleteNodeEnd(d_list *list);
+int deleteNode(d_list *list, unsigned int index);
+void freeNode(d_node *node);
+void freeList(d_list *list);
+void destroyList(d_list *list);
+
+#endif
