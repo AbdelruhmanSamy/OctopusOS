c9b144887f412fe49a2ac5e0495a77762771adf6
Abdelruhman Samy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-04-13 01:50:20 +0200

    feat: minHeap added

---
+++ code/minHeap.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * @list: array of data
+ * @size: number of elements in the heap
+ * @compare: 
+ returns: 
+  -  1: element1 > element2
+  - -1: element1 < element1
+  -  0: element1 = element2
+  
+ * Description: minimum heap structure
+*/
+typedef struct min_heap{
+    size_t capacity;      
+    void** arr;        
+    size_t size;
+    int (*compare)(void* e1, void* e2);
+}min_heap;
+
+min_heap* createMinHeap(int comp(void* , void* ));  //returns pointer on the heap to start using it
+void insertMinHeap(min_heap** heap , void* element);                                  //insert new element in the heap
+void* extractMin(min_heap* heap);                                  //get the min element
+void decreaseKey(min_heap* heap ,int ind);          //shift-up operation, called on decreasing the priority value
+void minHeapify(min_heap* heap , int ind);            
+min_heap** doubleCapacity(min_heap* heap);
+void swap(void** arr, int ind1, int ind2);
+void printHeap(min_heap* heap); 
