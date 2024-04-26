48329ace31e7ce1f189a4e47d0d15ca7affe99dd
amir-kedis <amir.kedis@gmail.com>
2024-04-26 11:46:19 +0300

    fix: processes not continuing

--- code/process.c
+++ code/process.c
@@ -1,26 +1,32 @@
 #include "headers.h"
-#include "structs.h"
 #include "scheduler.h"
+#include "structs.h"
 
-int* shmAdd;
+int *shmAdd;
 int shmid;
 
-void sigTermHandler(int signum){
-  if(shmdt(shmAdd) == -1){
+void sigTermHandler(int signum) {
+  if (shmdt(shmAdd) == -1) {
     perror("Error in detach\n");
-  }
-  else if(DEBUG){
-    printf("process %d detached from memory sucessfully\n" , getpid());
+  } else if (DEBUG) {
+    printf("process %d detached from memory sucessfully\n", getpid());
   }
 
-  if(shmctl(shmid , IPC_RMID , (struct shmid_ds*) 0)== -1 ){
+  if (shmctl(shmid, IPC_RMID, (struct shmid_ds *)0) == -1) {
     perror("error in deleting shared memory");
-  }  
+  }
+
+  // FIXME: Samy look at these 2 lines
+  // I added them because process was reaching return 0 and making the exit
+  // cleanup which kills the clock and so the whole app
+  // make sure it doesn't affect the process
+  printf("In process %d, Terminating\n", getpid());
+  raise(SIGKILL);
 }
 
 int main(int agrc, char *argv[]) {
 
-  printf("In process with pid = %d\n" , getpid());
+  printf("In process with pid = %d\n", getpid());
   fflush(stdout);
 
   signal(SIGINT, clearResources);
@@ -30,31 +36,31 @@ int main(int agrc, char *argv[]) {
     perror("atexit");
     exit(1);
   }
-    
-    shmid = initSchProShm(getpid());
-    shmAdd = (int*)shmat(shmid , (void*)0 , 0);
-    
-    if(DEBUG)
-      printf("In process %d , RT = %d\n" ,getpid(), *shmAdd);
 
-  fflush(stdout);  
+  shmid = initSchProShm(getpid());
+  shmAdd = (int *)shmat(shmid, (void *)0, 0);
+
+  if (DEBUG)
+    printf("In process %d , RT = %d\n", getpid(), *shmAdd);
+
+  fflush(stdout);
   initClk();
 
   int preTime = getClk();
   while (*shmAdd > 0) {
     int currTime = getClk();
-    
-    if(currTime != preTime){
+
+    if (currTime != preTime) {
       preTime = currTime;
 
       (*shmAdd)--;
-      
-      if(DEBUG)
-        printf("In process %d, RT = %d \n" , getpid() , *shmAdd);
+
+      if (DEBUG)
+        printf("In process %d, RT = %d \n", getpid(), *shmAdd);
     }
   }
 
-  kill(getppid() , SIGUSR1);
+  kill(getppid(), SIGUSR1);
 
   destroyClk(false);
 
